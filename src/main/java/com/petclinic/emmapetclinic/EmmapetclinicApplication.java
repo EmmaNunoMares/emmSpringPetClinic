1174b197bc9d121075dc2d600ac846fcb2642f96
EmmaNunoMares <emmanuelnunomares@gmail.com>
2020-03-22 16:37:57 -0600

    first commit

---
+++ src/main/java/com/petclinic/emmapetclinic/EmmapetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.petclinic.emmapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmapetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmapetclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petclinic/emmapetclinic/EmmapetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petclinic.emmapetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmapetclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
